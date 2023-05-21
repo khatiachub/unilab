3360e3dfe420cb3363233cafd77b785af31995b6
khatiachub <101997849+khatiachub@users.noreply.github.com>
2023-05-21 23:33:03 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import './styled.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
 function App() {
+  const[state,setState]=useState([])
+  const handleClick=()=>{
+    setState([...state,[]])
+  }
+  const undoClick=()=>{
+
+  }
+  const redoClick=()=>{
+
+  }
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      setState({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', handleClick);
+
+    return () => {
+      window.removeEventListener(
+        'mousemove',
+        handleClick
+      );
+    };
+  }, []);
 
   return (
-    <div className='background'>
+    <div onClick={handleClick} className='background'>
+      <button onClick={undoClick}>undo</button>
+      <button onClick={redoClick}>redo</button>
+      {state.map((item,i)=>{
+        <div key={i}  className='ball'>{item}</div>
+      })}
       
     
     </div>
